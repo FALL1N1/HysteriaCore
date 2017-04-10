918062c856ec3269bad460c19e8a9835523016ac
JunkyBulgaria <emiliyan9604bg@gmail.com>
2017-04-11 02:14:32 +0300

    Core/Spells: Shadowmourne's Soul Fragments will now proc in bg/arenas as well as it's supposed to be.

--- src/server/scripts/Spells/spell_item.cpp
+++ src/server/scripts/Spells/spell_item.cpp
@@ -2455,8 +2455,8 @@ class spell_item_shadowmourne : public SpellScriptLoader
                     return false;
 
                 // Xinef: Not on BG / Arena
-                if (!GetTarget()->FindMap() || GetTarget()->FindMap()->IsBattlegroundOrArena())
-                    return false;
+                //if (!GetTarget()->FindMap() || GetTarget()->FindMap()->IsBattlegroundOrArena())
+                    //return false;
 
                 if (const SpellInfo* procSpell = eventInfo.GetDamageInfo()->GetSpellInfo())
                     if (!eventInfo.GetDamageInfo()->GetDamage())
