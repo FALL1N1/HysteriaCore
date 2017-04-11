dd3e342d260e1665829271c8c52d3d1595797069
JunkyBulgaria <emiliyan9604bg@gmail.com>
2017-04-11 07:49:36 +0300

    Core/Commands: Implement 3 custom .firefly commands for our beta.

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -83,6 +83,11 @@ public:
         };
         static ChatCommand commandTable[] =
         {
+            // firefly custom begin
+            { "firefly damage",     SEC_ADMINISTRATOR,      false, &HandleFireFlyDamage,                "", NULL },
+            { "firefly maxlevel",   SEC_ADMINISTRATOR,      false, &HandleFireFlyMaxLevel,              "", NULL },
+            { "firefly minlevel",   SEC_ADMINISTRATOR,      false, &HandleFireFlyMinLevel,              "", NULL },
+            // firefly custom end
             { "arena",              SEC_ADMINISTRATOR,      true,  NULL,                                "", arenaCommandTable },
             { "fake",               SEC_ADMINISTRATOR,      true,  NULL,                                "", fakeCommandTable },
             { "dev",                SEC_ADMINISTRATOR,      false, &HandleDevCommand,                   "", NULL },
@@ -144,6 +149,44 @@ public:
             { NULL,                 0,                      false, NULL,                                "", NULL }
         };
         return commandTable;
+    }
+
+    static bool HandleFireFlyDamage(ChatHandler* handler, char const* args)
+    {
+        if (Player* plr = handler->GetSession()->GetPlayer())
+            if (!plr->HasAura(68378) || !plr->HasAura(64238) || !plr->HasAura(62555) || !plr->HasAura(62560))
+            {
+                plr->AddAura(68378, plr);
+                plr->AddAura(64238, plr);
+                plr->AddAura(62555, plr);
+                plr->AddAura(62560, plr);
+                handler->PSendSysMessage("Your damage has been buffed, if you don't need that much simply right-click the auras we have added you.");
+            }
+            else handler->PSendSysMessage("Please remove all of your damage modifiers before using this command.");
+
+        return true;
+    }
+
+    static bool HandleFireFlyMinLevel(ChatHandler* handler, char const* args)
+    {
+        if (Player* plr = handler->GetSession()->GetPlayer())
+        {
+            plr->SetLevel(1);
+            handler->PSendSysMessage("Your level has been set to 1, if you want to be 80 please use the command \".firefly maxlevel\".");
+        }
+
+        return true;
+    }
+
+    static bool HandleFireFlyMaxLevel(ChatHandler* handler, char const* args)
+    {
+        if (Player* plr = handler->GetSession()->GetPlayer())
+        {
+            plr->SetLevel(80);
+            handler->PSendSysMessage("Your level has been set to 80, if you want to be 1 please use the command \".firefly minlevel\".");
+        }
+
+        return true;
     }
 
     static bool HandleSkirmishCommand(ChatHandler* handler, char const* args)
