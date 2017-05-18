c0a218a324948d8d71ebf1585a6fb95c21dc0605
JunkyBulgaria <emiliyan9604bg@gmail.com>
2017-05-18 04:27:19 +0300

    Core/Scripts: Forgot to remove the outdated kodo script.

--- src/server/scripts/Kalimdor/zone_desolace.cpp
+++ src/server/scripts/Kalimdor/zone_desolace.cpp
@@ -424,96 +424,6 @@ class npc_cork_gizelton : public CreatureScript
         };
 };
 
-
-// Theirs
-enum DyingKodo
-{
-    SAY_SMEED_HOME                  = 0,
-
-    QUEST_KODO                      = 5561,
-
-    NPC_SMEED                       = 11596,
-    NPC_AGED_KODO                   = 4700,
-    NPC_DYING_KODO                  = 4701,
-    NPC_ANCIENT_KODO                = 4702,
-    NPC_TAMED_KODO                  = 11627,
-
-    SPELL_KODO_KOMBO_ITEM           = 18153,
-    SPELL_KODO_KOMBO_PLAYER_BUFF    = 18172,
-    SPELL_KODO_KOMBO_DESPAWN_BUFF   = 18377,
-    SPELL_KODO_KOMBO_GOSSIP         = 18362
-
-};
-
-class npc_aged_dying_ancient_kodo : public CreatureScript
-{
-public:
-    npc_aged_dying_ancient_kodo() : CreatureScript("npc_aged_dying_ancient_kodo") { }
-
-    struct npc_aged_dying_ancient_kodoAI : public ScriptedAI
-    {
-        npc_aged_dying_ancient_kodoAI(Creature* creature) : ScriptedAI(creature) {}
-
-        void JustRespawned()
-        {
-            me->UpdateEntry(RAND(NPC_AGED_KODO, NPC_DYING_KODO, NPC_ANCIENT_KODO), NULL, false);
-        }
-
-        void MoveInLineOfSight(Unit* who)
-        {
-            if (who->GetEntry() == NPC_SMEED && me->IsWithinDistInMap(who, 10.0f) && !me->HasAura(SPELL_KODO_KOMBO_GOSSIP))
-            {
-                me->SetHomePosition(me->GetPositionX(), me->GetPositionY(), me->GetPositionZ(), me->GetOrientation());
-                me->GetMotionMaster()->Clear();
-
-                DoCast(me, SPELL_KODO_KOMBO_GOSSIP, true);
-                if (Creature* smeed = who->ToCreature())
-                    smeed->AI()->Talk(SAY_SMEED_HOME);
-            }
-        }
-
-        void SpellHit(Unit* caster, SpellInfo const* spell)
-        {
-            if (spell->Id == SPELL_KODO_KOMBO_ITEM)
-            {
-                if (!(caster->HasAura(SPELL_KODO_KOMBO_PLAYER_BUFF) || me->HasAura(SPELL_KODO_KOMBO_DESPAWN_BUFF))
-                    && (me->GetEntry() == NPC_AGED_KODO || me->GetEntry() == NPC_DYING_KODO || me->GetEntry() == NPC_ANCIENT_KODO))
-                {
-                    me->UpdateEntry(NPC_TAMED_KODO, NULL, false);
-                    EnterEvadeMode();
-                    me->GetMotionMaster()->MoveFollow(caster, PET_FOLLOW_DIST, me->GetFollowAngle());
-
-                    caster->CastSpell(caster, SPELL_KODO_KOMBO_PLAYER_BUFF, true);
-                    DoCast(me, SPELL_KODO_KOMBO_DESPAWN_BUFF, true);
-                }
-            }
-            else if (spell->Id == SPELL_KODO_KOMBO_GOSSIP)
-            {
-                me->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
-                me->DespawnOrUnsummon(60000);
-            }
-        }
-    };
-
-    bool OnGossipHello(Player* player, Creature* creature)
-    {
-        if (player->HasAura(SPELL_KODO_KOMBO_PLAYER_BUFF) && creature->HasAura(SPELL_KODO_KOMBO_DESPAWN_BUFF))
-        {
-            player->TalkedToCreature(creature->GetEntry(), 0);
-            player->RemoveAurasDueToSpell(SPELL_KODO_KOMBO_PLAYER_BUFF);
-        }
-
-        player->SEND_GOSSIP_MENU(player->GetGossipTextId(creature), creature->GetGUID());
-        return true;
-    }
-
-    CreatureAI* GetAI(Creature* creature) const
-    {
-        return new npc_aged_dying_ancient_kodoAI(creature);
-    }
-
-};
-
 /*######
 ## npc_dalinda_malem. Quest 1440
 ######*/
@@ -622,7 +532,6 @@ void AddSC_desolace()
     new npc_cork_gizelton();
 
     // Theirs
-    new npc_aged_dying_ancient_kodo();
     new npc_dalinda();
     new go_demon_portal();
 }
