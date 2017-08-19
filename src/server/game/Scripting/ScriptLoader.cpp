94e6501a8539b3ac0d130fb92693d9ab03bee030
Kartselyanski <emiliyan9604bg@gmail.com>
2017-08-20 00:42:52 +0300

    custom script file

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -26,6 +26,9 @@ void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
 void AddSC_example_commandscript();
 
+// load custom scripts
+void AddSC_custom_scripts();
+
 // spells
 void AddSC_deathknight_spell_scripts();
 void AddSC_druid_spell_scripts();
@@ -1264,5 +1267,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
     AddSC_CrossFactionGroups();
+    AddSC_custom_scripts();
 #endif
 }
---
+++ src/server/scripts/Custom/custom_scripts.cpp
@@ -0,0 +1,4 @@
+void AddSC_custom_scripts()
+{
+    
+}
