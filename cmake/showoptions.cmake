647383c14b2ab635345c21ab3933b320d5af4201
Kartselyanski <emiliyan9604bg@gmail.com>
2017-10-08 17:33:34 +0300

    hopefully finished with all cmake junk

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -8,7 +8,7 @@ message("")
 
 # output information about installation-directories and locations
 
-message("* Install core to        : ${CMAKE_INSTALL_PREFIX}")
+#message("* Install core to        : ${CMAKE_INSTALL_PREFIX}")
 if( UNIX )
   message("* Install libraries to   : ${LIBSDIR}")
   message("* Install configs to     : ${CONF_DIR}")
