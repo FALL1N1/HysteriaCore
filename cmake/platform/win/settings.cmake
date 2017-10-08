ee5b6ae285bb8744b9ddf9d28fef9cda5145f1dd
Kartselyanski <emiliyan9604bg@gmail.com>
2017-10-08 16:00:41 +0300

    cmake misc

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -4,12 +4,6 @@ set(ACE_LIBRARY "ace")
 set(BZIP2_LIBRARIES "bzip2")
 set(ZLIB_LIBRARIES "zlib")
 
-add_definitions(-D_WIN32_WINNT=0x0601)
-
-if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc/settings.cmake)
-elseif (CMAKE_CXX_PLATFORM_ID MATCHES "MinGW")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/mingw/settings.cmake)
-elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/clang/settings.cmake)
 endif()
+
+include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc/settings.cmake)
