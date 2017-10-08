bae17ee71e88512c3f55165e72abee65615a399a
Kartselyanski <emiliyan9604bg@gmail.com>
2017-10-08 16:10:33 +0300

    some pb changes

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -10,13 +10,9 @@
 
 option(SERVERS          "Build worldserver and authserver"                            1)
 option(SCRIPTS          "Build core with scripts included"                            1)
-option(TOOLS            "Build map/vmap/mmap extraction/assembler tools"              0)
 option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"              1)
 option(USE_COREPCH      "Use precompiled headers when compiling servers"              1)
 option(ELUNA            "Build Eluna Lua Engine"                                      0)
 option(WITH_WARNINGS    "Show all warnings during compile"                            0)
 option(WITH_COREDEBUG   "Include additional debug-code in core"                       0)
-set(WITH_SOURCE_TREE    "hierarchical" CACHE STRING "Build the source tree for IDE's.")
-set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical hierarchical-folders)
-option(WITH_MESHEXTRACTOR "Build meshextractor (alpha)"                               0)
 option(WITHOUT_GIT      "Disable the GIT testing routines"                            0)
--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -3,7 +3,4 @@ set(ACE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/acelite)
 set(ACE_LIBRARY "ace")
 set(BZIP2_LIBRARIES "bzip2")
 set(ZLIB_LIBRARIES "zlib")
-
-endif()
-
 include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc/settings.cmake)
