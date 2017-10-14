5d2bd6adc3b00e94eb7cf68283730a9e8dbfa671
Kartselyanski <emiliyan9604bg@gmail.com>
2017-10-14 23:57:40 +0300

    lost

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -12,6 +12,7 @@ option(SERVERS          "Build worldserver and authserver"                      
 option(SCRIPTS          "Build core with scripts included"                            1)
 option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"              1)
 option(USE_COREPCH      "Use precompiled headers when compiling servers"              1)
+option(TOOLS            "Build map/vmap/mmap extraction/assembler tools"              0)
 option(ELUNA            "Build Eluna Lua Engine"                                      0)
 option(WITH_WARNINGS    "Show all warnings during compile"                            0)
 option(WITH_COREDEBUG   "Include additional debug-code in core"                       0)
